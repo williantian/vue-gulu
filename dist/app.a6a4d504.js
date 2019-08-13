fa55b77ac1a40520827f20663804f4b37b7a740c
Your Name <you@example.com>
2019-08-13 18:07:37 +0800

    添加icon 和 iconPosition 属性

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12414,7 +12414,16 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+//
+var _default = {
+  props: ['icon', 'iconPosition']
+};
 exports.default = _default;
         var $c35cd9 = exports.default || module.exports;
       
@@ -12425,10 +12434,26 @@ exports.default = _default;
         /* template */
         Object.assign($c35cd9, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "g-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon", attrs: { "aria-hidden": "true" } }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12508,7 +12533,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59421" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49912" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/Button.vue
+++ src/Button.vue
@@ -1,8 +1,17 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
 <style lang="scss">
   .g-button {
@@ -12,14 +21,19 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus{
-      outline: none;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: top; /*解决左右icon的button不对齐 上下不对齐加上即可*/
+    &:hover {border-color: var(--border-color-hover);}
+    &:active {background-color: var(--button-active-bg);}
+    &:focus{outline: none;}
+    > .content {order: 2;}
+    > .icon {order: 1; margin-right:0.1em;}
+    &.icon-right {
+      > .content {order: 1;}
+      > .icon {order: 2; margin-right: 0;margin-left: 0.1em;}
     }
   }
+
 </style>
