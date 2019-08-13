56ebfa81f227c8509bad19a2979da56abf506b09
Your Name <you@example.com>
2019-08-13 19:03:37 +0800

    完成icon.vue 完善样式

--- src/Button.vue
+++ src/Button.vue
@@ -1,8 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <svg v-if="icon" class="icon" aria-hidden="true">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -18,7 +16,6 @@
         default: 'left',
         //属性检查器
         validator (value){
-          console.log(value);
           return value !== 'left' || value !== 'right';
         },
       }
---
+++ src/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg class="g-icon" aria-hidden="true">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+<script>
+  export default {
+    props: ['name']
+  }
+</script>
+<style lang="scss">
+  .g-icon {
+   width: 1em; height: 1em;
+   vertical-align: middle;
+   fill: currentColor;
+   overflow: hidden;
+  }
+</style>
