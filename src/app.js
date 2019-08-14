6972a39039ff6d74297c4b02f0b897cafb50e3de
Your Name <you@example.com>
2019-08-14 12:25:51 +0800

    使用 chai.expect 添加测试用例

--- src/app.js
+++ src/app.js
@@ -9,6 +9,85 @@ Vue.component('g-button-group', ButtonGroup);
  new Vue({
   el: '#app',
   data: {
-    message: 'hello world'
+    loading1: false,
   }
 });
+
+ //单元测试
+import chai from 'chai'
+const expect = chai.expect;
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  });
+  vm.$mount('#test');
+ let useElement = vm.$el.querySelector('use');
+  expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading:true,
+    }
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector('use');
+  expect(useElement.getAttribute('xlink:href')).to.equal('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  });
+  vm.$mount(div);
+  let svg = vm.$el.querySelector('svg');
+  let {order} = window.getComputedStyle(svg);
+  expect(order).to.equal('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  });
+  vm.$mount(div);
+  let svg = vm.$el.querySelector('svg');
+  let {order} = window.getComputedStyle(svg);
+  expect(order).to.equal('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+//使用mock
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  });
+  let n = 1
+  vm.$mount();
+  vm.$on('click',()=> n=2)
+  let button = vm.$el;
+  button.click()
+  expect(n).to.equal(2)
+}
