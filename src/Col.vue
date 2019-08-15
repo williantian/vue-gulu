be6de1d39abee7ee73bdb3b247d3a13a7f80e053
Your Name <you@example.com>
2019-08-15 13:30:46 +0800

    基本实现row和col的功能

--- src/Col.vue
+++ src/Col.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+  :style="{paddingLeft: gutter/2+'px' , paddingRight: gutter/2+'px'}"
+  >
+    <div style="border:1px solid green ;height: 100px">
     <slot></slot>
+    </div>
   </div>
 </template>
 <script>
   export default {
   	name: 'GuluCol',
     props: {
-  		span: [Number,String]
-    }
-  }
+  		span: {
+  			type: [Number,String],
+      },
+      offset: {
+  			type:[Number,String]
+      }
+    },
+    data (){
+  		return {
+  			gutter: 0
+      }
+    },
+	}
 </script>
 <style lang="scss" scoped>
   .col {
     height: 100px;
-    background: grey;
     width: 50%;
-    border: 1px solid red;
-
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
          width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
