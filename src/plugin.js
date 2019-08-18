beaceff116922c15e2601b1a638c9d8d1ee088e7
Your Name <you@example.com>
2019-08-18 13:04:58 +0800

    实现只能有一个toast

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,20 @@
 import Toast from "./Toast";
+function createToast ({Vue,message,propsData}){
+	let Constructor = Vue.extend(Toast)
+	let toast = new Constructor({propsData})
+	toast.$slots.default = [message] //必须是一个数组
+	toast.$mount()
+	document.body.appendChild(toast.$el)
+	return toast
+}
+let currentToast
 export default {
 	install(Vue , options){
 		Vue.prototype.$toast = function(message,toastOptions){
-			let Constructor = Vue.extend(Toast)
-			let toast = new Constructor({
-				propsData: toastOptions
-			})
-			console.log(toast)
-			toast.$slots.default = [message] //必须是一个数组
-			toast.$mount()
-			document.body.appendChild(toast.$el)
+			if(currentToast){
+				currentToast.close()
+			}
+			currentToast = createToast({Vue,message, propsData: toastOptions})
 		}
 	}
 }
