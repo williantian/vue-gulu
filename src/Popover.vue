a2d9315bfd166da79946b0256844b9ef3ef1b99f
Your Name <you@example.com>
2019-08-19 18:19:37 +0800

    解决抖动bug

--- src/Popover.vue
+++ src/Popover.vue
@@ -161,10 +161,12 @@
         left: 10px;
       }
       &::before {
+        border-bottom: none;
         border-top-color: black;
         top: 100%;
       }
       &::after {
+        border-bottom: none;
         border-top-color: white;
         top: calc(100% - 1px);
       }
@@ -175,10 +177,12 @@
         left: 10px;
       }
       &::before {
+        border-top: none;
         border-bottom-color: black;
         bottom: 100%;
       }
       &::after {
+        border-top: none;
         border-bottom-color: white;
         bottom: calc(100% - 1px);
       }
@@ -191,10 +195,12 @@
         top: 50%;
       }
       &::before {
+        border-right: none;
         border-left-color: black;
         left: 100%;
       }
       &::after {
+        border-right: none;
         border-left-color: white;
         left: calc(100% - 1px);
       }
@@ -206,10 +212,12 @@
         top: 50%;
       }
       &::before {
+        border-left: none;
         border-right-color: black;
         right: 100%;
       }
       &::after {
+        border-left: none;
         border-right-color: white;
         right: calc(100% - 1px);
       }
